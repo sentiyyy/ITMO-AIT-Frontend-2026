c82111a5c6236b6f2e3b3301cd910f3a68405e98
xlevando <xlevando@gmail.com>
2026-03-10 14:35:56 +0300

    feat: fix ticket buying logic

--- J3211/Левандо Ксения/labs/lab1/js/events.js
+++ J3211/Левандо Ксения/labs/lab1/js/events.js
@@ -1,14 +1,20 @@
 function buyTicket(eventName, eventDate) {
+    if (localStorage.getItem("auth") !== "true") {
+    alert("Please login to buy tickets");
+    window.location.href = "login.html";
+    return;
+}
+    const user = JSON.parse(localStorage.getItem("user"));
+    let tickets = JSON.parse(localStorage.getItem("tickets")) || [];
 
-let tickets = JSON.parse(localStorage.getItem("tickets")) || [];
+    tickets.push({
+    event: eventName,
+    date: eventDate,
+    owner: user.email
+    });
 
-tickets.push({
-event: eventName,
-date: eventDate
-});
+    localStorage.setItem("tickets", JSON.stringify(tickets));
 
-localStorage.setItem("tickets", JSON.stringify(tickets));
-
-alert("Ticket purchased!");
+    alert("Ticket purchased!");
 
 }
